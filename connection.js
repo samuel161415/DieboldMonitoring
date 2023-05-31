8acb0198b1916cefd32bdb0d6946c683b6e95801
sami <samuelnegalign1@gmail.com>
2023-05-31 12:17:36 +0300

    test

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
         mongoose.connect(process.env.MONGO_URL) 
         console.log('connected');
     } catch(error) {
-        console.log(error)
+         console.log(error)
         process.exit()
     }
 }
